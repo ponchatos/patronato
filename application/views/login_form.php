e6f3d42867d3d0a63e2892ae1a26fb22881991c2
Jorge Luis Davalos Ruelas <Jorge Luis Davalos Ruelas>
2016-05-03 16:46:13 -0600

    [xxx]

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,69 +1,49 @@
+<!DOCTYPE html>
 <html>
-<?php
-if (isset($this->session->userdata['logged_in'])) {
-
-  header("location: ".base_url()."/administracion/");
-}
-?>
-<head>
-  <title>Login Form</title>
-  <base href="/"/>
-  <link rel="stylesheet" type="text/css" href="<?php echo '/LostCitiesSite/assets/css/bootstrap.min.css'?>" >
-  <link rel="stylesheet" type="text/css" href="<?php echo '/LostCitiesSite/assets/css/css.css'?>" >
-  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
-  <script src="<?php echo '/LostCitiesSite/assets/js/jquery.js'?>"></script>
-  <script src="<?php echo '/LostCitiesSite/assets/js/bootstrap.min.js'?>"></script>
-  <!--<script src="<?php //echo '/LostCitiesSite/assets/js/js.js'?>"></script>-->
-
-</head>
-<body>  
- <div class="container">
-  
-  <div class="row" id="pwd-container">
-    <div class="col-md-4"></div>
-    
-    <div class="col-md-4">
-      <section class="login-form">
-  <?php 
-  $attr = array('role' => 'login');
-    echo form_open('user_authentication/user_login_process',$attr); 
-  if (isset($logout_message)) {
-    echo "<div class='message'>";
-    echo $logout_message;
-    echo "</div>";
-  }
-  if (isset($x)) {
-    echo "<div class='message'>";
-    echo $message_display;
-    echo "</div>";
-  }
-    
-  ?>
   <?php
-    echo "<div class='error_msg'>";
-    if (isset($error_message)) {
-      echo $error_message;
-    }
-    echo validation_errors();
-    echo "</div>";
-  ?>
-  <!--Se crea un form donde se introduzca el username y la contraseña, se envía a la funcion user_login_process-->
-  <img src="/LostCitiesSite/assets/img/LostCitiesLogo.png" class="img-responsive" alt="" />
-  <input type="text" name="username" placeholder="Usuario" required class="form-control input-lg" />
-  <input type="password" class="form-control input-lg" name="password" id="password" placeholder="Password" required="" minlength="6"/>
-  <button type="submit" name="submit" class="btn btn-lg btn-success btn-block">Ingresar</button>
-  <div>
-    <a href="/LostCitiesSite/user_authentication/user_registration_show">Crear Cuenta</a>
+  if (isset($this->session->userdata['logged_in'])) {
+
+    header("location: ".base_url()."administracion/");
+  }?>
+
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    <title>Login</title>
+  </head>
+
+  <body>
+    <div id="barra"></div>
+
+  <div class="col-md-3 center-block quitar-float text-center espacioArriba">
+    <img src="css/images/PATRONATO.jpg">
   </div>
-  <?php echo form_close(); ?>
-  <div class="form-links">
-          <a href="<?php echo base_url()?>"><?php echo base_url() ?></a>
+
+  <div class="col-md-3 center-block quitar-float text-center espacioArriba">
+
+    <form >
+
+      <fieldset>
+          <legend>Iniciar Sesión</legend>
+              <label>Usuario:</label>
+                  <input id="campo1" name="userName" type="text" /><br>
+              <label>Contraseña:</label>
+                  <input id="campo2" name="password" type="text" />
+              <input id="campo3" name="enviar" type="submit" value="Enviar" />
+      </fieldset>
+
+  </form>
   </div>
-  </section>
-  </div>
-  <div class="col-md-4"></div>
-  </div>
-  </div>
+    
+  
+      
+
+    
+
+  
 
 </body>
+
 </html>
