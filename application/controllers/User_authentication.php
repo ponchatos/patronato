32c6dbc428c645960e70f610afab7d928f1112ed
Alfonso Sotelo <vgcwebmaster@gmail.com>
2016-05-03 14:03:43 -0600

    cambios 1.0

---
+++ application/controllers/User_authentication.php
@@ -0,0 +1,35 @@
+<?php
+
+//session_start(); //we need to start session in order to access it through CI
+
+Class User_Authentication extends CI_Controller {
+
+public function __construct() {
+parent::__construct();
+
+// Load form helper library
+$this->load->helper('form');
+
+// Load form validation library
+$this->load->library('form_validation');
+
+// Load session library
+$this->load->library('session');
+
+// Load database
+$this->load->model('login_database');
+
+// Load Metodos
+$this->load->model('metodos');
+}
+
+public function index(){
+
+}
+
+
+
+
+}
+
+?>
